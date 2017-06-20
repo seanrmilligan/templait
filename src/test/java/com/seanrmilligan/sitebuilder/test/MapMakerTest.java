ec0fc8fb2cfa1e24caf3fffb7a506627cc3544cf
Sean R Milligan <seanrmilligan@gmail.com>
2017-06-19 23:17:35 -0400

    Add directory layout for unit tests

---
+++ src/test/java/com/seanrmilligan/sitebuilder/test/MapMakerTest.java
@@ -0,0 +1,14 @@
+package com.seanrmilligan.sitebuilder.test;
+
+/**
+ * Created by sean on 6/19/17.
+ */
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MapMakerTest {
+	@Test
+	public void testMap() {
+		assertEquals(6, 6);
+	}
+}
