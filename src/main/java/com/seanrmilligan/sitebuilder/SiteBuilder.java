c3985ed9cced462a8bc96c4e47099222b5dac8dc
Sean R Milligan <seanrmilligan@gmail.com>
2017-06-23 18:26:57 -0400

    remove helper function, use Strings class instead of hardcoded strings

--- src/main/java/com/seanrmilligan/sitebuilder/SiteBuilder.java
+++ src/main/java/com/seanrmilligan/sitebuilder/SiteBuilder.java
@@ -11,53 +11,52 @@ import com.seanrmilligan.utils.Action;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import static com.seanrmilligan.sitebuilder.view.Strings.APPLICATION_NAME;
+import static com.seanrmilligan.sitebuilder.view.Strings.LOAD_SITE_BUTTON_TEXT;
+import static com.seanrmilligan.sitebuilder.view.Strings.NEW_SITE_BUTTON_TEXT;
+
 public class SiteBuilder extends Application {
 	SiteBuilderWindow gui;
-	SiteDataController siteDataController;
 
 	/**
 	 * @param primaryStage This application's window.
 	 */
 	@Override
 	public void start(Stage primaryStage) {
-		this.siteDataController = new SiteDataController();
-		Site site = this.startup(primaryStage);
-
+		Site site = null;
+		
+		StartWindow launcherView = new StartWindow(primaryStage);
+		launcherView.setTitleBar(APPLICATION_NAME);
+		launcherView.setButtonText(NEW_SITE_BUTTON_TEXT, LOAD_SITE_BUTTON_TEXT);
+		launcherView.showAndWait();
+		Action action = launcherView.getAction();
+		
+		SiteDataController siteDataController = new SiteDataController();
+		
+		switch (action) {
+			case NEW:
+				site = siteDataController.newSite(primaryStage);
+				break;
+			case LOAD:
+				try {
+					site = siteDataController.loadSite(primaryStage);
+				} catch (Exception e) {
+					System.out.println(e.toString());
+					System.out.println(e.getMessage());
+				}
+				break;
+		}
+		
 		if (site != null) {
 			this.gui = new SiteBuilderWindow(primaryStage);
 			this.gui.setSiteName(site.getName());
 			this.gui.setSiteDomain(site.getDomain());
-			this.gui.init("Site Builder");
+			this.gui.init(APPLICATION_NAME);
 		}
 	}
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		launch(args);
-	}
-
-    public Site startup(Stage primaryStage) {
-		Site site = null;
-        StartWindow launcherView = new StartWindow(primaryStage);
-        launcherView.init("Site Builder");
-		launcherView.showAndWait();
-		Action action = launcherView.getAction();
-		
-		switch (action) {
-			case NEW:
-				site = this.siteDataController.newSite(primaryStage);
-				break;
-			case LOAD:
-				try {
-					site = this.siteDataController.loadSite(primaryStage);
-				} catch (Exception e) {
-					System.out.println(e.toString());
-				}
-				break;
-			default:
-				System.err.println("Invalid action: " + action.toString());
-		}
-		
-		return site;
 	}
 }
---
+++ src/main/java/com/seanrmilligan/sitebuilder/view/Strings.java
@@ -0,0 +1,11 @@
+package com.seanrmilligan.sitebuilder.view;
+
+/**
+ * Created by sean on 6/23/17.
+ */
+public class Strings {
+	public static final String APPLICATION_NAME = "Site Builder";
+	public static final String NEW_SITE_BUTTON_TEXT = "New Site";
+	public static final String LOAD_SITE_BUTTON_TEXT = "Load Site";
+	
+}
