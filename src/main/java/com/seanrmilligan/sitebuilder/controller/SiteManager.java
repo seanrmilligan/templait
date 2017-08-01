d73462220fc54c834a7d272fdd38b3c73970a211
Sean R Milligan <seanrmilligan@gmail.com>
2017-08-01 17:01:52 -0400

    SiteManager overhall, repetitive actions now all outsourced to helper methods

--- src/main/java/com/seanrmilligan/sitebuilder/controller/SiteManager.java
+++ src/main/java/com/seanrmilligan/sitebuilder/controller/SiteManager.java
@@ -21,21 +21,11 @@ public class SiteManager {
 	private static final String JSON_KEY_SITE_DOMAIN = "siteDomain";
 	private static final String JSON_KEY_SITE_SUBDOMAINS = "subdomains";
 	
-	public static void newSite(File projDir, Site site) throws NullPointerException, NotDirectoryException, FileAlreadyExistsException, FileNotFoundException, IOException {
-		File sbDir;
-		
-		if (projDir == null) {
-			throw new NullPointerException("Project directory is null.");
-		}
-		
-		if (!projDir.isDirectory()) {
-			throw new NotDirectoryException("Path is not a directory: " + projDir.getAbsolutePath());
-		}
-		
-		sbDir = getSBDir(projDir);
+	public static void newSite(File projDir, Site site) throws NullPointerException, IOException {
+		File sbDir = constructFileFromPath(projDir.getAbsolutePath(), SiteManager.PROJECT_DIRECTORY);
 		
 		if (sbDir.exists()) {
-			throw new FileAlreadyExistsException("Site Builder directory found: " + sbDir.getAbsolutePath());
+			throw new FileAlreadyExistsException("Cannot make directory: " + PROJECT_DIRECTORY + " already exists.");
 		}
 		
 		if (!sbDir.mkdir()) {
@@ -45,46 +35,12 @@ public class SiteManager {
 		saveSite(projDir, site);
 	}
 	
-	public static Site loadSite(File projDir) throws NullPointerException, FileNotFoundException, IOException {
-		Site site = null;
-		File sbDir = null, projFile = null;
-		FileInputStream stream;
-		JSONObject project;
-		
-		if (projDir == null) {
-			throw new NullPointerException("Project directory is null.");
-		}
-
-		// search through the directory the user choose to see if the site builder settings directory is inside
-		for (File item : projDir.listFiles()) {
-			if (item.isDirectory() && item.getName().equals(SiteManager.PROJECT_DIRECTORY)) {
-				sbDir = item;
-				break;
-			}
-		}
-
-		// no site builder settings directory found; this is not a site builder managed project
-		if (sbDir == null) {
-			throw new FileNotFoundException("Site Builder directory not found: " + PROJECT_DIRECTORY);
-		}
-
-		// get the project json file from in the site builder settings dir
-		for (File item : sbDir.listFiles()) {
-			if (item.isFile() && item.getName().equals(PROJECT_DATA)) {
-				projFile = item;
-				break;
-			}
-		}
-		
-		if (projFile == null) {
-			throw new FileNotFoundException("File not found: " + PROJECT_DATA);
-		}
-		
-		stream = new FileInputStream(projFile);
+	public static Site loadSite(File projDir) throws NullPointerException, IOException {
+		File projFile = getProjFile(projDir);
+		FileInputStream stream = new FileInputStream(projFile);
 		JSONTokener parser = new JSONTokener(stream);
-		project = (JSONObject) parser.nextValue();
-		
-		site = new Site(project.getString(JSON_KEY_SITE_NAME), project.getString(JSON_KEY_SITE_DOMAIN));
+		JSONObject project = (JSONObject) parser.nextValue();
+		Site site = new Site(project.getString(JSON_KEY_SITE_NAME), project.getString(JSON_KEY_SITE_DOMAIN));
 		
 		if (project.has(JSON_KEY_SITE_SUBDOMAINS)) {
 			JSONArray subdomains = project.getJSONArray(JSON_KEY_SITE_SUBDOMAINS);
@@ -99,15 +55,11 @@ public class SiteManager {
 		return site;
 	}
 	
-	public static void saveSite(File projDir, Site site) throws FileNotFoundException, IOException {
-		File projFile;
-		FileOutputStream stream;
+	public static void saveSite(File projDir, Site site) throws IOException {
+		File projFile = getProjFile(projDir);
+		FileOutputStream stream = new FileOutputStream(projFile);
 		JSONObject project = new JSONObject();
 		JSONArray subdomains = new JSONArray();
-		
-		projFile = getProjFile(projDir);
-		
-		stream = new FileOutputStream(projFile);
 		
 		project.put(JSON_KEY_SITE_NAME, site.getName());
 		project.put(JSON_KEY_SITE_DOMAIN, site.getDomain());
@@ -119,31 +71,48 @@ public class SiteManager {
 		project.put(JSON_KEY_SITE_SUBDOMAINS, subdomains);
 		
 		stream.write(project.toString().getBytes());
+		
+		stream.close();
 	}
 	
-	private static File getSBDir(File projDir) {
-		File sbDir;
-		String projPath = projDir.getAbsolutePath();
-		
-		if (projPath.endsWith(File.separator)) {
-			sbDir = new File(projPath + SiteManager.PROJECT_DIRECTORY);
+	private static File getSBDir(File projDir) throws FileNotFoundException, NotDirectoryException {
+		if (projDir == null) {
+			throw new NullPointerException("Project directory is null.");
+		} else if (!projDir.isDirectory()) {
+			throw new NotDirectoryException("Path is not a directory: " + projDir.getAbsolutePath());
 		} else {
-			sbDir = new File(projPath + File.separator + SiteManager.PROJECT_DIRECTORY);
+			String projPath = projDir.getAbsolutePath();
+			File sbDir = constructFileFromPath(projPath, SiteManager.PROJECT_DIRECTORY);
+			
+			if (!sbDir.exists()) {
+				throw new FileNotFoundException("Site Builder directory not found: " + sbDir.getAbsolutePath());
+			}
+			
+			return sbDir;
 		}
-		
-		return sbDir;
 	}
 	
-	private static File getProjFile(File projDir) {
-		File projFile;
-		String sbPath = getSBDir(projDir).getAbsolutePath();
+	private static File getProjFile(File projDir) throws FileNotFoundException, NotDirectoryException {
+		// getSBDir validates projDir and throws the appropriate exceptions
+		String sbPath = getSBDir(projDir).getAbsolutePath();;
+		File projFile = constructFileFromPath(sbPath, SiteManager.PROJECT_DATA);;
 		
-		if (sbPath.endsWith(File.separator)) {
-			projFile = new File(sbPath + SiteManager.PROJECT_DATA);
-		} else {
-			projFile = new File(sbPath + File.separator + SiteManager.PROJECT_DATA);
+		if (!projFile.exists()) {
+			throw new FileNotFoundException("File not found: " + projFile.getAbsolutePath());
 		}
 		
 		return projFile;
+	}
+	
+	private static File constructFileFromPath(String path, String filename) {
+		File file;
+		
+		if (path.endsWith(File.separator)) {
+			file = new File(path + filename);
+		} else {
+			file = new File(path + File.separator + filename);
+		}
+		
+		return file;
 	}
 }
